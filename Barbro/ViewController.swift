8388301baf2170fa14127b42153ddbca5d3d9782
Kaushal Topinkatti B <kaushalbt@icloud.com>
2018-12-19 13:37:21 +0530

    Initial Commit

---
+++ Barbro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Barbro
+//
+//  Created by Kaushal Topinkatti B on 19/12/18.
+//  Copyright © 2018 Kaushal Topinkatti B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BarbroTests/BarbroTests.swift
@@ -0,0 +1,34 @@
+//
+//  BarbroTests.swift
+//  BarbroTests
+//
+//  Created by Kaushal Topinkatti B on 19/12/18.
+//  Copyright © 2018 Kaushal Topinkatti B. All rights reserved.
+//
+
+import XCTest
+@testable import Barbro
+
+class BarbroTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
